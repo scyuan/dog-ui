6ffe11f79e35efcb5d0502b8eaacb4be57c7dd0d
scyuan <644397334@qq.com>
2019-05-31 20:39:59 +0800

    alert transition

--- examples/docs/alert.md
+++ examples/docs/alert.md
@@ -27,7 +27,25 @@
 
 带图标
 
-<d-alert icon="md-checkmark-circle">默认提示的文案</d-alert>
-<d-alert icon type="success">成功提示的文案</d-alert>
-<d-alert icon type="info">信息提示的文案</d-alert>
-<d-alert icon type="danger">错误提示的文案</d-alert>
+<d-alert icon="md-alert">默认提示的文案</d-alert>
+<d-alert icon="md-checkmark-circle" type="success">成功提示的文案</d-alert>
+<d-alert icon="md-alert" type="info">信息提示的文案</d-alert>
+<d-alert icon="md-close-circle" type="danger">错误提示的文案</d-alert>
+
+带关闭文字
+
+
+<d-alert icon="md-alert" closable closeText="Close">默认提示的文案</d-alert>
+<d-alert icon="md-checkmark-circle" type="success" closable closeText="Close">成功提示的文案</d-alert>
+
+带关闭按钮
+
+<d-alert icon="md-alert" type="info" closable>信息提示的文案</d-alert>
+<d-alert icon="md-close-circle" type="danger" closable>错误提示的文案</d-alert>
+
+带标题
+
+<d-alert icon="md-alert" closable title="默认提示的文案">描述文字描述文字描述文字描述文字描述文字</d-alert>
+<d-alert icon="md-checkmark-circle" type="success" title="成功提示的文案">描述文字描述文字描述文字描述文字描述文字</d-alert>
+<d-alert icon="md-alert" closable type="info" title="信息提示的文案">描述文字描述文字描述文字描述文字描述文字</d-alert>
+<d-alert icon="md-close-circle" closable type="danger" title="错误提示的文案">描述文字描述文字描述文字描述文字描述文字</d-alert>
--- packages/alert/src/alert.vue
+++ packages/alert/src/alert.vue
@@ -1,19 +1,39 @@
 <template>
-  <div
-    :class="['d-alert',_type?`d-alert-${_type}`:'',_border?`d-alert-${_border}`:'',_center?'is-alert-center':'']"
-  >
-    <i class="d-icon" :class="_icon" v-if="_icon"></i>
-    <slot></slot>
-  </div>
+  <transition name="alert-fade" mode="in-out">
+    <div
+      v-if="visible"
+      :class="['d-alert',_type?`d-alert-${_type}`:'',_border?`d-alert-${_border}`:'',_center?'is-alert-center':'']"
+    >
+      <i class="d-icon" :class="_icon" v-if="_icon"></i>
+      <i
+        class="d-alert-btn"
+        :class="closeText?'d-alert-btn_close':'d-icon md-close d-alert_icon'"
+        v-if="closable"
+        @click="close"
+      >{{ closeText }}</i>
+      <div class="d-alert_title" v-if="title">{{ title }}</div>
+      <div :class="['d-alert_content',title?'has-alert-title':'']">
+        <slot name="default"></slot>
+      </div>
+    </div>
+  </transition>
 </template>
 <script>
 export default {
   name: "DAlert",
   props: {
-    border: String,
+    border: Boolean,
     type: String,
-    center: String,
-    icon: String
+    center: Boolean,
+    icon: String,
+    closable: Boolean,
+    closeText: String,
+    title: String
+  },
+  data() {
+    return {
+      visible: true
+    };
   },
   computed: {
     _type() {
@@ -23,31 +43,28 @@ export default {
     _border() {
       const types = ["success", "danger", "info"];
       if (types.indexOf(this.type) > -1) {
-        return this.border != undefined &&
-          (this.border === "" || this.border.length > 0)
-          ? `border_${this.type}`
-          : "";
+        return this.border ? `border_${this.type}` : "";
       } else {
-        return this.border != undefined &&
-          (this.border === "" || this.border.length > 0)
-          ? `border`
-          : "";
+        return this.border ? `border` : "";
       }
     },
     _center() {
-      return this.center != undefined &&
-        (this.center === "" || this.center.length > 0)
-        ? "center"
-        : "";
+      return this.center ? "center" : "";
     },
     _icon() {
       return this.icon ? this.icon : "";
+    }
+  },
+  methods: {
+    close() {
+      this.visible = false;
+      this.$emit("close");
     }
   }
 };
 </script>
 <style lang="less" scoped>
-@import "../../them-chalk/alert.less";
 @import "../../them-chalk/icon.less";
+@import "../../them-chalk/alert.less";
 </style>
 
